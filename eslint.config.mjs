7e329d0b2a53aa995cc9d973a09f9df74ad70bc1
Byron2016 <bgva2005@yahoo.com>
2024-10-03 15:49:13 -0500

    add scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,41 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  {
+    plugins: {
+      json,
+      markdown,
+    },
+  },
+  // lint JSON files
+  {
+    files: ["**/*.json"],
+    language: "json/json",
+    rules: {
+      "json/no-duplicate-keys": "error",
+      // "no-irregular-whitespace": "off",
+    },
+  },
+  // lint MARKDOWN files
+  {
+    files: ["**/*.md"],
+    plugins: {
+      markdown,
+    },
+    language: "markdown/commonmark",
+    rules: {
+      "markdown/no-html": "error",
+    },
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
